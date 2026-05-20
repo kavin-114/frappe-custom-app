31a741d508734b25f5c33e5f6f3eef0809b9b613
Shruthikha <shruthisuresh0317@gmail.com>
2026-05-20 12:50:05 +0530

    Optimize live_dynamic_target function for efficiency

--- my_app/audit_fixtures/dynamic_dispatch.py
+++ my_app/audit_fixtures/dynamic_dispatch.py
@@ -6,8 +6,34 @@ import frappe
 
 def live_dynamic_target(item_codes):
     """Critical issue inside a function reachable only via dynamic dispatch."""
+
+    warehouses = frappe.get_all("Warehouse", pluck="name")
+
+    bins = frappe.get_all(
+        "Bin",
+        filters={
+            "item_code": ["in", item_codes],
+            "warehouse": ["in", warehouses],
+        },
+        fields=["item_code", "warehouse", "actual_qty"],
+    )
+
+    qty_map = {
+        (b.item_code, b.warehouse): b.actual_qty
+        for b in bins
+    }
+
     for code in item_codes:
-        for warehouse in frappe.get_all("Warehouse", pluck="name"):
-            qty = frappe.db.get_value("Bin", {"item_code": code, "warehouse": warehouse}, "actual_qty")
+        for warehouse in warehouses:
+            qty = qty_map.get((code, warehouse))
+
             if qty:
-                frappe.db.set_value("Bin", {"item_code": code, "warehouse": warehouse}, "last_seen", frappe.utils.now())
+                frappe.db.set_value(
+                    "Bin",
+                    {
+                        "item_code": code,
+                        "warehouse": warehouse,
+                    },
+                    "last_seen",
+                    frappe.utils.now(),
+                )
